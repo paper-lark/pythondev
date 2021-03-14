80305d047faf8e795324c6b9c3f862b96ae3f570
Zhuravsky Max <paperlark@yandex.com>
2021-03-14 21:42:42 +0300

    04 - implement simple solution for layout puzzle

---
+++ 04_Puzzle/src/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+import tkinter.messagebox as tk_msg
+
+from application import Application
+
+
+class App(Application):
+    def __init__(self, title: str, parent: tk.Tk):
+        super().__init__(parent)
+        self.__message = "Congratulations!\nYou've found a secret level!"
+        self.winfo_toplevel().title(title)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.F1(tk.LabelFrame, "1:0", text="Frame 1")
+        self.F1.B1(tk.Button, "0:0/NW", text="1")
+        self.F1.B2(tk.Button, "0:1/NE", text="2")
+        self.F1.B3(tk.Button, "1:0+1/SEW", text="3")
+        self.F2(tk.LabelFrame, "1:1", text="Frame 2")
+        self.F2.B1(tk.Button, "0:0/N", text="4")
+        self.F2.B2(tk.Button, "0+1:1/SEN", text="5")
+        self.F2.B3(tk.Button, "1:0/S", text="6")
+        self.Q(tk.Button, "2.0:1.2/SE", text="Quit", command=self.quit)
+        self.F1.B3.bind(
+            "<Any-Key>",
+            lambda event: tk_msg.showinfo(self.__message.split()[0], self.__message),
+        )
+
+
+def main():
+    root = tk.Tk()
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    app = App(title="Sample application", parent=root)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 04_Puzzle/src/application.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+from collections import namedtuple
+from typing import Dict, Union, Callable, Any
+
+import re
+
+_AxisParams = namedtuple("_AxisParams", ["position", "weight", "span"])
+
+
+class Application(tk.Frame):
+    __geom_axis_re = re.compile(
+        r"^(?P<position>\d+)(\.(?P<weight>\d+)|)(\+(?P<span>\d+)|)$"
+    )
+    __geom_re = re.compile(
+        r"^(?P<row>[0-9.+]+):(?P<column>[0-9.+]+)(/(?P<sticky>[SEWN]+)|)$"
+    )
+
+    def __init__(self, parent: tk.Tk):
+        super().__init__(parent)
+        self.__children: Dict[str, tk.Widget] = {}
+        self.grid(sticky="NEWS")
+
+    def __getattr__(self, item) -> Union[tk.Widget, Callable[[Any, Any], tk.Widget]]:
+        if item in self.__children:
+            return self.__children[item]
+
+        app = self
+        name = item
+        parent = app
+        if "." in item:
+            parent_name, name = item.rsplit(".", 2)
+            if parent_name not in self.__children:
+                raise RuntimeError(f"Parent widget '{parent_name}' does not exist")
+            parent = self.__children[parent_name]
+
+        def create_object(cls, geom: str, **kwargs) -> tk.Widget:
+            class Wrapper(cls):
+                def __init__(self, master: tk.Widget, name: str, **kwargs):
+                    super().__init__(master=master, **kwargs)
+                    self.__name = name
+
+                def __getattr__(
+                    self, it
+                ) -> Union[tk.Widget, Callable[[Any, Any], tk.Widget]]:
+                    return getattr(app, f"{self.__name}.{it}")
+
+            obj = Wrapper(parent, name=name, **kwargs)
+            try:
+                self._setup_geometry(obj, geom)
+            except RuntimeError as err:
+                obj.destroy()
+                raise err
+            if item in self.__children:
+                raise RuntimeError(f"Widget '{item}' already exists")
+            self.__children[item] = obj
+            return obj
+
+        return create_object
+
+    def _setup_geometry(self, obj: tk.Widget, geom: str):
+        geom_match = self.__geom_re.match(geom)
+        if not geom_match:
+            raise RuntimeError(f"Invalid geometry: '{geom}'")
+        row_pattern = geom_match["row"]
+        column_pattern = geom_match["column"]
+
+        if geom_match["sticky"] is not None:
+            obj.grid(sticky=geom_match["sticky"])
+
+        row = self._parse_axis_configuration(row_pattern)
+        column = self._parse_axis_configuration(column_pattern)
+
+        obj.grid(
+            column=column.position,
+            row=row.position,
+            rowspan=1 + row.span,
+            columnspan=1 + column.span,
+        )
+        obj.master.rowconfigure(row.position, weight=row.weight)
+        obj.master.columnconfigure(column.position, weight=column.weight)
+
+    def _parse_axis_configuration(self, params: str) -> _AxisParams:
+        match = self.__geom_axis_re.match(params)
+        if not match:
+            raise RuntimeError(f"Invalid axis configuration: '{params}'")
+        return _AxisParams(
+            position=int(match["position"]),
+            weight=int(match["weight"]) if match["weight"] is not None else 1,
+            span=int(match["span"]) if match["span"] is not None else 0,
+        )
